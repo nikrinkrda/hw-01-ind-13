3964504c0222b6370abe2a9ddcc61319c63e1c82
Nik <nik_rink@ukr.net>
2023-05-28 23:29:55 +0300

    Изминения для создания новой ветки

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Привет! Добро пожаловать!");
+        System.out.println("Я новая ветка!!!");
 
         for (int i = 1; i <= 5; i++) {
 
